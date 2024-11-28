f6ff9cf7d47d9fdcc2b24f79477ee566429b4881
Luc <lmj543@york.ac.uk>
2024-11-28 17:28:08 +0000

    fixed offset and size of stadium building

--- UniSim/core/src/main/java/io/github/unisim/ui/BuildingMenu.java
+++ UniSim/core/src/main/java/io/github/unisim/ui/BuildingMenu.java
@@ -18,6 +18,7 @@ import io.github.unisim.Point;
 import io.github.unisim.building.Building;
 import io.github.unisim.building.BuildingType;
 import io.github.unisim.world.World;
+
 import java.util.ArrayList;
 
 /**
@@ -32,68 +33,68 @@ public class BuildingMenu {
   private ArrayList<Building> buildings = new ArrayList<>();
   private ArrayList<Image> buildingImages = new ArrayList<>();
   private Label buildingInfoLabel = new Label(
-      "", new Skin(Gdx.files.internal("ui/uiskin.json"))
+    "", new Skin(Gdx.files.internal("ui/uiskin.json"))
   );
   private Table buildingInfoTable = new Table();
 
   /**
    * Create a Building Menu and attach its actors and components to the provided stage.
    * Also handles drawing buildings and their flipped variants
-
+   *
    * @param stage - The stage on which to draw the menu.
    */
   public BuildingMenu(Stage stage, World world) {
     this.world = world;
     // Set building images and sizes
     buildings.add(new Building(
-        new Texture(Gdx.files.internal("buildings/restaurant.png")),
-        0.01f,
-        new Vector2(0.35f, -0.9f),
-        new Point(),
-        new Point(3, 3),
-        false,
-        BuildingType.EATING,
-        "Canteen"
+      new Texture(Gdx.files.internal("buildings/restaurant.png")),
+      0.01f,
+      new Vector2(0.35f, -0.9f),
+      new Point(),
+      new Point(3, 3),
+      false,
+      BuildingType.EATING,
+      "Canteen"
     ));
     buildings.add(new Building(
-        new Texture(Gdx.files.internal("buildings/library.png")),
-        0.0075f,
-        new Vector2(1.8f, -4.6f),
-        new Point(),
-        new Point(20, 12),
-        false,
-        BuildingType.LEARNING,
-        "Library"
+      new Texture(Gdx.files.internal("buildings/library.png")),
+      0.0075f,
+      new Vector2(1.8f, -4.6f),
+      new Point(),
+      new Point(20, 12),
+      false,
+      BuildingType.LEARNING,
+      "Library"
     ));
     buildings.add(new Building(
-        new Texture(Gdx.files.internal("buildings/basketballCourt.png")),
-        0.0025f,
-        new Vector2(1f, -2.4f),
-        new Point(),
-        new Point(6, 9),
-        false,
-        BuildingType.RECREATION,
-        "Basketball Court"
+      new Texture(Gdx.files.internal("buildings/basketballCourt.png")),
+      0.0025f,
+      new Vector2(1f, -2.4f),
+      new Point(),
+      new Point(6, 9),
+      false,
+      BuildingType.RECREATION,
+      "Basketball Court"
     ));
-      buildings.add(new Building(
-          new Texture(Gdx.files.internal("buildings/stadium.png")),
-          0.0025f,
-          new Vector2(1f, -2.4f),
-          new Point(),
-          new Point(6, 9),
-          false,
-          BuildingType.RECREATION,
-          "Stadium"
-      ));
     buildings.add(new Building(
-        new Texture(Gdx.files.internal("buildings/studentHousing.png")),
-        0.108f,
-        new Vector2(1.4f, -2.8f),
-        new Point(),
-        new Point(11, 11),
-        false,
-        BuildingType.SLEEPING,
-        "Student Accomodation"
+      new Texture(Gdx.files.internal("buildings/stadium.png")),
+      0.0025f,
+      new Vector2(1f, 1f),
+      new Point(),
+      new Point(12, 16),
+      false,
+      BuildingType.RECREATION,
+      "Stadium"
+    ));
+    buildings.add(new Building(
+      new Texture(Gdx.files.internal("buildings/studentHousing.png")),
+      0.108f,
+      new Vector2(1.4f, -2.8f),
+      new Point(),
+      new Point(11, 11),
+      false,
+      BuildingType.SLEEPING,
+      "Student Accomodation"
     ));
 
     table = new Table();
@@ -131,8 +132,8 @@ public class BuildingMenu {
 
   /**
    * Called when the window is resized, scales the building menu images with the window size.
-
-   * @param width - The new width of the window in pixels
+   *
+   * @param width  - The new width of the window in pixels
    * @param height - The new height of the window in pixels
    */
   @SuppressWarnings("unchecked")
@@ -147,9 +148,9 @@ public class BuildingMenu {
       Image buildingImage = (Image) (cell.getActor());
       Vector2 textureSize = new Vector2(buildingImage.getWidth(), buildingImage.getHeight());
       cell.width(
-          height * 0.1f * (textureSize.x < textureSize.y ? textureSize.x / textureSize.y : 1)
+        height * 0.1f * (textureSize.x < textureSize.y ? textureSize.x / textureSize.y : 1)
       ).height(
-          height * 0.1f * (textureSize.y < textureSize.x ? textureSize.y / textureSize.x : 1)
+        height * 0.1f * (textureSize.y < textureSize.x ? textureSize.y / textureSize.x : 1)
       );
     }
 
