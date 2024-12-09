e679388abec0a32b4591f4f220ea0e6d16d930de
Luc <lmj543@york.ac.uk>
2024-12-09 13:44:12 +0000

    added all possible achievements

--- UniSim/core/src/main/java/io/github/unisim/events/Fire.java
+++ UniSim/core/src/main/java/io/github/unisim/events/Fire.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.graphics.Texture;
 import io.github.unisim.achievements.OnFire;
 import io.github.unisim.building.Building;
 import io.github.unisim.building.BuildingManager;
-import io.github.unisim.ui.BuildingMenu;
 
 /**
  * Negative event that removes all bonuses of the 'on fire' building.
