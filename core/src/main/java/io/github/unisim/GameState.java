8f5b505037577cc8d34f8f0bfac9a0a613d234d0
Mike Vaux <19553364+mikevaux@users.noreply.github.com>
2024-12-13 11:05:36 +0000

    Move buildable tiles out of GameState altogether

--- core/src/main/java/io/github/unisim/GameState.java
+++ core/src/main/java/io/github/unisim/GameState.java
@@ -8,9 +8,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import io.github.unisim.ui.GameScreen;
 import io.github.unisim.ui.SettingsScreen;
 import io.github.unisim.ui.StartMenuScreen;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Contains a collection of settings and references that should be available globally.
@@ -21,11 +18,6 @@ public class GameState {
   public static Screen startScreen = new StartMenuScreen();
   public static Screen settingScreen = new SettingsScreen();
   public static Screen currentScreen;
-  // Create an unmodifiable set containing the IDs of all buildable tiles
-  // we use a set to make searching more efficient
-  public static Set<Integer> buildableTiles = Stream.of(
-      14, 15).collect(Collectors.toUnmodifiableSet()
-  );
 
   ////////////////////////
   // Below is the beginning of a move toward a Singleton pattern for GameState. The current setup is causing problems.
