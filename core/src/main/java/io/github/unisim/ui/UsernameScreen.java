4de464e47b918b6c0a5bc89f047ed32444bafa0e
Luc <lmj543@york.ac.uk>
2024-12-18 23:28:23 +0000

    added documentation, and a  variable to store the username

--- core/src/main/java/io/github/unisim/ui/UsernameScreen.java
+++ core/src/main/java/io/github/unisim/ui/UsernameScreen.java
@@ -10,15 +10,20 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import io.github.unisim.GameState;
 
+/**
+ * __NEW: WHOLE CLASS__ username screen that allows a user to enter
+ * a username to be displayed on the leaderboard.
+ */
 public class UsernameScreen implements Screen {
   private Stage stage;
   private Table table;
   private TextField usernameField;
   private TextButton leaderboardButton;
   private InputMultiplexer inputMultiplexer = new InputMultiplexer();
+  private String username;
 
   /**
-   * Create a new Settings screen and draw the initial UI layout.
+   * Create a new UsernameScreen and draw the initial UI layout.
    */
   public UsernameScreen() {
     stage = new Stage();
@@ -26,7 +31,7 @@ public class UsernameScreen implements Screen {
     Skin skin = GameState.getInstance().getDefaultSkin();
 
     // Username text field
-    usernameField = new TextField("Please Enter A Username", skin);
+    usernameField = new TextField("Enter Username", skin);
 
     // Leaderboard button
     leaderboardButton = new TextButton("Enter", skin);
@@ -35,8 +40,8 @@ public class UsernameScreen implements Screen {
     leaderboardButton.addListener(new ClickListener() {
       @Override
       public void clicked(InputEvent event, float x, float y) {
+        username = usernameField.getText();
         // will lead to leaderboard screen in future
-        System.out.println("yay");
       }
     });
 
