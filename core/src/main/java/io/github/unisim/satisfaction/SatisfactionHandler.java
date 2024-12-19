65e78b232cc60ea8a4007d6fc2bbf2e86a942c87
Luc <lmj543@york.ac.uk>
2024-12-19 14:23:02 +0000

    modified the post game bonuses to only be added if enough buildings are placed

--- core/src/main/java/io/github/unisim/satisfaction/SatisfactionHandler.java
+++ core/src/main/java/io/github/unisim/satisfaction/SatisfactionHandler.java
@@ -54,33 +54,30 @@ public class SatisfactionHandler {
    */
   private int calculateBonus(){
     int satisfactionToAdd = 0;
-    final int smallBonus = 100;
-    final int largeBonus = 200;
+    final int smallBonus = 250;
+    final int largeBonus = 500;
     int sleepingNum = buildingManager.getNumberOf(BuildingType.SLEEPING);
     int learningNum = buildingManager.getNumberOf(BuildingType.LEARNING);
     int recreationNum = buildingManager.getNumberOf(BuildingType.RECREATION);
     int eatingNum = buildingManager.getNumberOf(BuildingType.EATING);
-    boolean allTrue = true;
-    //are there at least twice as many recreation buildings as food buildings?
-    if (recreationNum >= (2 * eatingNum)){
-      satisfactionToAdd += smallBonus;
-    }else{
-      allTrue = false;
+    boolean bothTrue = true;
+    // bonus only given if 10 or more buildings placed
+    if (learningNum + sleepingNum + recreationNum + eatingNum >= 10) {
+      //are there at least twice as many learning buildings as recreation and eating buildings?
+      if (learningNum >= (2 * recreationNum) && learningNum >= (2 * eatingNum)) {
+        satisfactionToAdd += smallBonus;
+      } else {
+        bothTrue = false;
+      }
+      //are there at least twice as many sleeping buildings as recreation and eating buildings?
+      if (sleepingNum >= (2 * recreationNum) && sleepingNum >= (2 * eatingNum)) {
+        satisfactionToAdd += smallBonus;
+      } else {
+        bothTrue = false;
+      }
     }
-    //are there at least twice as many learning buildings as recreation buildings?
-    if (learningNum >= (2 * recreationNum)){
-      satisfactionToAdd += smallBonus;
-    }else{
-      allTrue = false;
-    }
-    //are there at least twice as many sleeping buildings as learning buildings?
-    if (sleepingNum >= (2 * learningNum)){
-      satisfactionToAdd += smallBonus;
-    }else{
-      allTrue = false;
-    }
-    //have all bonuses been met?
-    if (allTrue){
+    //have both bonuses been met?
+    if (bothTrue){
       satisfactionToAdd += largeBonus;
     }
     return satisfactionToAdd;
